ffa7749c58097a9a44e2a9549255429e61a9e5ac
Eliasar YT <eliasarmoncada4@gmail.com>
2025-01-04 21:35:13 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,6 +24,8 @@ global.lenguaje = es //Predeterminado en idioma Español
 global.prefix = [`/`]
 
 //---------[ APIS GLOBAL ]---------
+global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({...query, ...(apikeyqueryname ? {[apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name]} : {})})) : '');
+global.apis = 'https://deliriussapi-oficial.vercel.app'
 global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']; 
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]; 
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']; 
