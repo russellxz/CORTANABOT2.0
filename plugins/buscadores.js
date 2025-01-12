44745d68dd1be1b1a5d7b4dde19387beb721fc31
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2025-01-12 01:43:20 -0400

    Update buscadores.js

--- plugins/buscadores.js
+++ plugins/buscadores.js
@@ -176,48 +176,9 @@ conn.sendMessage(m.chat, { audio: { url: texttospeechurl }, contextInfo: { "exte
 m.react('🗣️')}
 
 if (command == 'chatgpt' || command == 'ia') {
-const translate = require('@vitalets/google-translate-api') 
-const {Configuration, OpenAIApi} = require('openai') 
-const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key})
-const openaiii = new OpenAIApi(configuration)
-if (prefix == 'a' || prefix == 'A') return
-if (!text) return m.reply(`${lenguaje.lengua.ia} ${prefix + command} Recomienda un top 10 de películas de acción`) 
-try {
-conn.sendPresenceUpdate('composing', m.chat);
-let sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por elrebelde21, tu seras NovaBot-MD`;
-async function getOpenAIChatCompletion(texto) {
-const openaiAPIKey = global.openai_key;
-let chgptdb = global.chatgpt.data.users[m.sender];
-chgptdb.push({ role: 'user', content: texto });
-const url = "https://api.openai.com/v1/chat/completions";
-const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
-const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": "system", "content": sistema1 }, ...chgptdb, ]};
-const response = await fetch(url, {method: "POST", headers: headers, body: JSON.stringify(data)});
-const result = await response.json();
-const finalResponse = result.choices[0].message.content;
-return finalResponse;
-};
-let respuesta = await getOpenAIChatCompletion(text);
-if (respuesta == 'error' || respuesta == '' || !respuesta) return XD; // causar error undefined para usar otra api
-m.reply(`${respuesta}`.trim());
-} catch {
-try {
-const botIA222 = await openaiii.createCompletion({model: 'text-davinci-003', prompt: text, temperature: 0.3, max_tokens: 4097, stop: ['Ai:', 'Human:'], top_p: 1, frequency_penalty: 0.2, presence_penalty: 0});
-if (botIA222.data.choices[0].text == 'error' || botIA222.data.choices[0].text == '' || !botIA222.data.choices[0].text) return XD; // causar error undefined para usar otra api
-m.reply(botIA222.data.choices[0].text.trim());
-} catch {
-try {
-const syms1 = `Actuaras como un Bot de WhatsApp el cual fue creado por elrebelde21, tu seras NovaBot-MD`
-const Empireapi1 = await fetch(`https://api.cafirexos.com/api/chatgpt?text=${text}&name=${m.name}&prompt=${syms1}`);
-const empireApijson1 = await Empireapi1.json();
-if (empireApijson1.resultado == 'error' || empireApijson1.resultado == '' || !empireApijson1.resultado) return XD; // causar error undefined para lanzar msg de error
-m.reply(`${empireApijson1.resultado}`.trim());
-} catch {
-try {
+if (!text) return m.reply(`${lenguaje.lengua.ia} ${prefix + command} Recomienda un top 10 de películas de acción`)  
 let ia = await axios.get(`https://delirius-apiofc.vercel.app/ia/chatgpt?q=${text}`)
-await m.reply(ia.data.data)
-} catch {
-return m.reply(info.error)}}}}}
+await m.reply(ia.data.data)}
 
 if (command == 'bard' || command == 'ia2') { 
 if (prefix == 'a' || prefix == 'A') return
