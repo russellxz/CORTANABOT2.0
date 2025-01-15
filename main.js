c259267dd1af7bd818500652a2e4960fdd7dc17f
russellxz <162611099+russellxz@users.noreply.github.com>
2025-01-15 04:20:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -833,7 +833,7 @@ if (!isCreator) return reply(info.owner)
 
 
 //contador de chat 
-case '.listachat': {
+case 'listachat': {
     const fs = require('fs');
     const path = require('path');
     const DATA_FILE = path.join(__dirname, 'datoschat.json');
