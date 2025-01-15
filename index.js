3efa98202276e64080a2ad4cbad2ff55fb83fac9
russellxz <162611099+russellxz@users.noreply.github.com>
2025-01-15 04:45:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ return !color ? chalk.green(text) : color.startsWith('#') ? chalk.hex(color)(tex
 //no tocar ariba
 // Ruta del archivo para guardar los datos
 const path = require('path');
-const { saveChatData, loadChatData } = require('./main.js');
+
 // Ruta del archivo para guardar los datos
 const DATA_FILE = path.join(__dirname, 'datoschat.json');
 
