305ed4b85dfda589d4e3ec4a226ba00ec073caab
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2025-02-02 21:02:26 -0400

    Update buscadores.js

--- plugins/buscadores.js
+++ plugins/buscadores.js
@@ -14,7 +14,6 @@ const FormData = require("form-data")
 const os = require('os')
 
 async function buscadores(m, command, conn, text, budy, from, fkontak, prefix, args, q, quoted, lang, lolkeysapi) {
-if (global.db.data.users[m.sender].registered < true) return  conn.sendMessage(m.chat, {video: {url: verificar}, caption: info.registra}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 if (global.db.data.users[m.sender].banned) return
 if (command == 'yts' || command == 'ytsearch') {
 if (global.db.data.users[m.sender].level < 2) return m.reply(`${lenguaje['nivel']()} 2 ${lenguaje['nivel2']()} ${prefix}nivel`) 
