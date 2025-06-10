c5c508d56922bdc02d5df8b2570a61b45925aad5
russellxz <162611099+russellxz@users.noreply.github.com>
2025-06-10 01:28:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ async function perplexityQuery(q, prompt) {
   //lumi
   const axios = require("axios");
 const fetch = require("node-fetch");
-
+const path = require("path");
     const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion, makeCacheableSignalKeyStore } = require("@whiskeysockets/baileys");
     const chalk = require("chalk");
     const yargs = require('yargs/yargs')
