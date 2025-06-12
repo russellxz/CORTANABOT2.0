3b4f1243996fc66396f5f66a40d848d6de8601df
russellxz <162611099+russellxz@users.noreply.github.com>
2025-06-12 00:03:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -82,8 +82,9 @@ async function perplexityQuery(q, prompt) {
   const axios = require("axios");
 const fetch = require("node-fetch");
 const { cargarSubbots } = require("./indexsubbots");
-
-  
+const { cargarSubbots } = require("./indexsubbots");
+require("./subbotWatcher"); // ⬅️ Detecta subbots nuevos automáticamente
+   
     const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion, makeCacheableSignalKeyStore } = require("@whiskeysockets/baileys");
     const chalk = require("chalk");
     const yargs = require('yargs/yargs')
