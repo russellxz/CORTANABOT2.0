171147a1cdd19378399aad8a88970fa6f0adc929
russellxz <162611099+russellxz@users.noreply.github.com>
2025-07-10 23:47:13 -0400

    Update Delbots.js

--- plugins/Delbots.js
+++ plugins/Delbots.js
@@ -1,20 +1,29 @@
 const fs = require("fs");
 const path = require("path");
+const { subBots } = require("../indexsubbots");
 
 const handler = async (msg, { conn }) => {
   const number = msg.key?.participant || msg.key.remoteJid;
   const sessionDir = path.join(__dirname, "../subbots");
   const sessionPath = path.join(sessionDir, number);
 
+  const index = subBots.indexOf(sessionPath);
+  if (index !== -1) {
+    subBots.splice(index, 1);
+  }
+
   if (fs.existsSync(sessionPath)) {
     fs.rmSync(sessionPath, { recursive: true, force: true });
     await conn.sendMessage(msg.key.remoteJid, {
-      text: `🗑️ *Tu sesión ha sido eliminada correctamente.*\n\nPuedes volver a usar *#sercode o #code* cuando gustes.`}, {quoted: msg});
+      text: "🗑️ *Tu sesión ha sido eliminada correctamente.*\n\nPuedes volver a usar *#sercode o #code* cuando gustes.",
+      quoted: msg,
+    });
     console.log(`✅ Carpeta del subbot ${number} eliminada por comando.`);
   } else {
     await conn.sendMessage(msg.key.remoteJid, {
-      text: `⚠️ *No se encontró ninguna carpeta activa para eliminar.*`
-    }, {quoted: msg});
+      text: "⚠️ *No se encontró ninguna carpeta activa para eliminar.*",
+      quoted: msg,
+    });
   }
 };
 
